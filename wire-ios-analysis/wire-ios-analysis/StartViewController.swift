1e2205621f2f2537f32a128c6eecfeaa125e1db2
DianaC18 <dc.cepeda@uniandes.edu.co>
2020-05-09 13:21:18 -0500

    Tab Bar

---
+++ wire-ios-analysis/wire-ios-analysis/StartViewController.swift
@@ -0,0 +1,22 @@
+//
+//  StartViewController.swift
+//  wire-ios-analysis
+//
+//  Created by Diana Cepeda on 9/05/20.
+//  Copyright © 2020 Juan Felipe Méndez. All rights reserved.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+
+    let image = UIImageView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view
+        
+    }
+    
+}
